37d8deb29dadcb8f88b1f47d7a7762699baca585
jjsskk <jinsukk9763@gmail.com>
2022-07-06 18:42:06 +0900

    파베 설정중

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Root from "./Router";
-
+import { useState } from "react";
+import firebase from "./firebase.config";
 
 function App() {
+    const auth = firebase.auth();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div className="App">
-            <Root />
+            <Root isLoggedIn={isLoggedIn} />
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "./firebase.config";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+console.log(firebase);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
